f94125d1df74a1036eb9d5d284e0a534290b0246
Bjari <116325415+fajar-i@users.noreply.github.com>
2023-11-28 12:26:33 +0700

    Add files via upload

---
+++ gdsc_web_dev/script.js
@@ -0,0 +1,8 @@
+console.log("Ini adalah console log");
+console.info("Ini adalah console info");
+console.warn("ini adalah console warn");
+console.error("ini adalah console error");
+
+function goToAnotherPage() {
+    window.location.href = 'https://www.upi.edu/';
+}
